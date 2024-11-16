cda6120fbfc860289618646fb7ea6738efd658bd
Kyser000 <118454721+Kyser000@users.noreply.github.com>
2024-11-16 21:05:56 +0100

    Ajout de composer

---
+++ SAE/SAE_dev_web_s3/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tayko/sae_dev_web_s3',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '86fb4809f6e0c47757a4adce59a341b6a31a3dd6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tayko/sae_dev_web_s3' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '86fb4809f6e0c47757a4adce59a341b6a31a3dd6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
